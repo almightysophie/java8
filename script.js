9bd7f116117c11168374d46bf1e713f8ce5b756a
almightysophie <77300753+almightysophie@users.noreply.github.com>
2021-01-26 15:34:35 -0500

    d

---
+++ script.js
@@ -0,0 +1,30 @@
+var vegetableType = null;
+
+vegetableType = prompt("What vegetable would you like?");
+
+if (vegetableType=="lettuce") {
+  console.log ("We don't get much call for that around here.");
+} else if (vegetableType=="napa"){ console.log ("Here is your " +vegetableType+ " !!");
+} else {console.log("Sorry, nonono.")};
+
+if (vegetableType=="potato") {
+    console.log ("We don't sell it.");
+  } else if (vegetableType=="napa"){ console.log ("Here is your " +vegetableType+ " !!");
+  } else {console.log("Sorry, nonono.")};
+
+
+
+  if (vegetableType=="tomato") {
+    console.log ("No longer in stock.");
+  } else if (vegetableType=="napa"){ console.log ("Here is your " +vegetableType+ " !!");
+  } else {console.log("Sorry, nonono.")};
+
+  if (vegetableType=="avocado") {
+    console.log ("Go find others.");
+  } else if (vegetableType=="napa"){ console.log ("Here is your " +vegetableType+ " !!");
+  } else {console.log("Sorry, nonono.")};
+
+  if (vegetableType=="bokchoy") {
+    console.log ("Out of stock");
+  } else if (vegetableType=="napa"){ console.log ("Here is your " +vegetableType+ " !!");
+  } else {console.log("Sorry, nonono.")};
